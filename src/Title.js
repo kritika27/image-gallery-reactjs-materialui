cad665422c50e3459780174415dbc44bed9775a2
kritika <kritikasri27@gmail.com>
2020-12-23 22:11:05 +0530

    Added Title and Search box

--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./index.css";
 
-const Title = () => {
+const Title = ({ onSearchChange, search }) => {
   return (
     <div className="title">
-      <h1>Travel Diaries</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ textAlign: "center", marginRight: "20px" }}>
+          Travel Diaries
+        </h1>
+        <input
+          style={{
+            border: "none",
+            outline: "none",
+            borderBottom: "2px solid blue",
+          }}
+          placeholder="Search"
+          type="search"
+          onChange={onSearchChange}
+          value={search}
+        />
+      </div>
       <h2>Europe Trip</h2>
       <p>
         "An Artist has no home in Europe except in Paris." -Friedrich Nietzsche
