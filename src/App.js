20bd2907989b95ee64ff521977d39d3b8039d192
kritika <kritikasri27@gmail.com>
2020-12-23 22:28:08 +0530

    Added main App page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Title";
 import UpdateForm from "./UpdateForm";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filterTodos = list.filter((todo) => {
+    return todo.title.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <Title />
-      <UpdateForm />
+    <div>
+      <Title onSearchChange={handleSearch} search={search} />
+
+      <UpdateForm list={list} filterTodos={filterTodos} setList={setList} />
     </div>
   );
 }
